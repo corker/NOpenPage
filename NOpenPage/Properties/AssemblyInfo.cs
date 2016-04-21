ac844df25c2538d109a15e0a8a30127ef545eb8c
Michael Borisov <corker@inbox.ru>
2016-04-21 21:21:07 +0200

    nuspec file added

--- NOpenPage/Properties/AssemblyInfo.cs
+++ NOpenPage/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NOpenPage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A page object pattern implementation for Selenium.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Borisov")]
 [assembly: AssemblyProduct("NOpenPage")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright Michael Borisov © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
